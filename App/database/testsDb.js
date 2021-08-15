82772407fc606d37992d3364df316e57a9deef0c
mdyErfani <mdyerfani@ut.ac.ir>
2021-08-15 21:01:34 +0430

    basic db providers

---
+++ App/database/testsDb.js
@@ -0,0 +1,12 @@
+const getGroups = (major) => [];
+const getBooks = (groupID) => [];
+const getChapters = (bookID) => [];
+const getSubjects = (cahpterID) => [];
+const getLearningTests = (subjectID) => [];
+
+const setGroup = (group) => {};
+const setBook = (book) => {};
+const setChapter = (chapter) => {};
+const setSubject = (subject) => {};
+const setLearningTest = (test) => {};
+const setHint = (hint) => {};
